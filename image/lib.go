f218b0c498122dc1cdd44e771a31298fa5a7391e
tzz <koi_town@outlook.com>
2026-06-22 00:10:34 +0800

    up

---
+++ image/lib.go
@@ -0,0 +1,22 @@
+package image
+
+import(
+	"image"
+)
+
+
+func ToRGBA(img image.Image) *image.RGBA {
+	rgba,ok := img.(*image.RGBA)
+	if ok{
+		return rgba
+	}
+
+	rgba = image.NewRGBA(img.Bounds())
+	for x := img.Bounds().Min.X; x < img.Bounds().Max.X; x++ {
+		for y := img.Bounds().Min.Y; y < img.Bounds().Max.Y; y++ {
+			rgba.Set(x, y, img.At(x, y))
+		}
+	}
+
+	return rgba
+}
